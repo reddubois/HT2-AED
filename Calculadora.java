e63cf580274b89edb46b236a3c35b36c7b6f9c58
reddubois <54864123+reddubois@users.noreply.github.com>
2022-02-03 11:53:30 -0600

    Cambio de nombres de variables
    Solo para simplificarme un poco la vida

--- Calculadora.java
+++ Calculadora.java
@@ -4,23 +4,23 @@ public class Calculadora implements ICalculadora {
 	
     int resultado;
     
-    public int sumar(int operando1, int operando2){
-        resultado = operando1 + operando2;  
+    public int sumar(int n1, int n2){
+        resultado = n1 + n2;  
         return resultado;
     }
     
-    public int restar(int operando1, int operando2){
-        resultado = operando1 - operando2;  
+    public int restar(int n1, int n2){
+        resultado = n1 - n2;  
         return resultado;
     }
     
-    public int multiplicar(int operando1, int operando2){
-        resultado = operando1 * operando2;  
+    public int multiplicar(int n1, int n2){
+        resultado = n1 * n2;  
         return resultado;
     }
     
-    public int dividir(int operando1, int operando2){
-        resultado = operando1 / operando2;  
+    public int dividir(int n1, int n2){
+        resultado = n1 / n2;  
         return resultado;
     }
     
--- ICalculadora.java
+++ ICalculadora.java
@@ -2,12 +2,12 @@ package com.uvg.main;
 
 public interface ICalculadora {
 
-    public int sumar(int operando1, int operando2);
+    public int sumar(int n1, int n2);
     
-    public int restar(int operando1, int operando2);
+    public int restar(int n1, int n2);
     
-    public int multiplicar(int operando1, int operando2);
+    public int multiplicar(int n1, int n2);
     
-    public int dividir(int operando1, int operando2);
+    public int dividir(int n1, int n2);
 	
 }
